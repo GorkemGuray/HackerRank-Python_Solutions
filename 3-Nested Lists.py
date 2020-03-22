bca1ca2ad7da1e86968cbefebb86f8515b7529c9
Görkem Güray <grkmgry@gmail.com>
2020-03-22 23:13:50 +0300

    Nested List Çözümü Eklendi

---
+++ 3-Nested Lists.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/nested-list/problem
+
+if __name__ == '__main__':
+    liste_1 = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        inner_list = [name,score]
+        liste_1.append(inner_list)
+
+    liste_1.sort(key = lambda x : x[1])
+
+    en_kucuk_sayi_adedi = 1 #aynı zamanda en küçük 2. sayının başlangıç indexi
+    en_kucuk_2_sayi_adedi = 1
+
+    for z in range ((len(liste_1)-1)):
+        if(liste_1[z][1]==liste_1[z+1][1]):
+            en_kucuk_sayi_adedi = en_kucuk_sayi_adedi+1
+        elif(liste_1[z][1]!=liste_1[z+1][1]):
+            break
+  
+
+    for j in range(en_kucuk_sayi_adedi, len(liste_1)-1):
+                 
+        if (liste_1[j][1] == liste_1[j+1][1]):
+            en_kucuk_2_sayi_adedi = en_kucuk_2_sayi_adedi+1
+        elif(liste_1[j][1] != liste_1[j+1][1]):
+            break
+
+    
+    e1 = en_kucuk_sayi_adedi
+    e2 = en_kucuk_sayi_adedi + en_kucuk_2_sayi_adedi
+
+    liste_2  = liste_1 [e1:e2]
+
+    liste_2.sort(key = lambda x:x[0])
+
+    for t in liste_2:
+        print(t[0])
